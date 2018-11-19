95a70e73eedb6e5807e09c870423cbda79f8e01f
cormobro <bonaet.felix@gmail.com>
2018-11-19 15:03:33 +0100

    ajout react7

---
+++ src/formulaire.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			poster: '',
+			comment: '',
+		}
+		this.onChange = this.onChange.bind(this);
+		this.submitForm = this.submitForm.bind(this);
+	}
+
+
+	onChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	}
+
+	submitForm(e) {
+		e.preventDefault();
+		       fetch("http://92.175.11.66:3001/api/quests/movies/", {
+                method: "POST",
+                headers: {
+                        "Content-Type": "application/json",
+                },
+                body: JSON.stringify(this.state),
+        })
+                .then(res => res.json())
+                .then(res => {
+                        if (res.error) {
+                                alert(res.error);
+                        } else {
+                                alert(`Film ajouté avec l'ID ${res}!`);
+                        }
+                }).catch(e => {
+                        console.error(e);
+                        alert('Erreur lors de l\'ajout d\'un film');
+                });
+
+	}
+
+	render() {
+		return (
+
+
+			<div className="FormEmployee">
+			<h1>Saisie du film</h1>
+
+			<form onSubmit={this.submitForm}>
+			<fieldset>
+			<legend>Informations</legend>
+			<div className="form-data">
+			<label htmlFor="moviename">Nom</label>
+			<input
+			type="text"
+			id="name"
+			name="name"
+			onChange={this.onChange}
+			value={this.state.name}
+			/>
+			</div>
+
+			<div className="form-data">
+			<label htmlFor="poster">Poster</label>
+			<input
+			type="text"
+			id="poster"
+			name="poster"
+			onChange={this.onChange}
+			value={this.state.poster}
+			/>
+			</div>
+
+			<div className="form-data">
+			<label htmlFor="comment">Commentaire</label>
+			<input
+			type="comment"
+			id="comment"
+			name="comment"
+			onChange={this.onChange}
+			value={this.state.comment}
+			/>
+			</div>
+			<hr />
+			<div className="form-data">
+			<input type="submit" value="Envoyer" />
+			</div>
+			</fieldset>
+			</form>
+			</div>
+		);
+	}
+}
+
+export default Form;
